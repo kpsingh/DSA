4e20a6418fc61e319adb30634055fcab6b1a6a50
Krishna Pratap Singh <kpsingh1234@gmail.com>
2022-02-02 16:52:03 +0530

    Agnostic BinarySearch

---
+++ src/com/practice/learning/AgnosticBinarySearch.java
@@ -0,0 +1,52 @@
+package com.practice.learning;
+
+public class AgnosticBinarySearch {
+    public static void main(String[] args) {
+
+        // int arr[] = {1, 3, 4, 6, 15, 17, 23, 34, 45, 56, 78, 89};
+
+        int arr[] = {45, 35, 25, 20, 9, 5, 3, -10, -20, -30};
+        int key = 56;
+        int index = agnosticBS(arr, key);
+        System.out.println(index);
+    }
+
+    static int agnosticBS(int[] arr, int key) {
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        // Check if given array is in ascending order or in descending order
+        boolean isAsc = arr[start] < arr[end];
+        if (isAsc) {
+            System.out.println("Ascending order...");
+        } else {
+            System.out.println("Descending order...");
+        }
+        while (start <= end) {
+
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] == key) {
+                return mid;
+            }
+
+            if (isAsc) {
+                if (arr[mid] < key) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            } else {
+                if (arr[mid] > key) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+
+        }
+
+        return -1;
+    }
+}
--- src/com/practice/learning/BinarySearch.java
+++ src/com/practice/learning/BinarySearch.java
@@ -3,26 +3,37 @@ package com.practice.learning;
 public class BinarySearch {
 
     public static void main(String[] args) {
-	// write your code here
-       System.out.println("Hello, World!");
+
+        int arr[] = {1, 3, 4, 6, 15, 17, 23, 34, 45, 56, 78, 89};
+        int key = 45;
+        int index = itreBinarySearch(arr, key);
+        System.out.println(index);
 
     }
 
-    public static int itreBinarySearch(int[] arr, int key){
+    public static int itreBinarySearch(int[] arr, int key) {
 
-    int start = 0;
-    int end = arr.length - 1;
-    int mid = (start + end ) / 2;
+        int start = 0;
+        int end = arr.length - 1;
 
+        while (start < end) {
+       /*
+        int mid = (start + end) / 2; this will may cause the int out of range in case start and end are very large
+        */
 
+            int mid = start + (end - start) / 2;
 
+            if (arr[mid] == key) {
+                return mid;
 
+            } else if (arr[mid] < key) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
 
-
-        retunn -1;
-
-
+        return -1;
     }
-
 
 }
