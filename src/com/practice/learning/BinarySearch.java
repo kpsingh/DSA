fd7cd308aa29ffb58f6bcf1bdaade1c4d8d9bb80
Krishna Pratap Singh <kpsingh1234@gmail.com>
2022-02-02 10:51:04 +0530

    BinarySearch Tutorials

---
+++ src/com/practice/learning/BinarySearch.java
@@ -0,0 +1,28 @@
+package com.practice.learning;
+
+public class BinarySearch {
+
+    public static void main(String[] args) {
+	// write your code here
+       System.out.println("Hello, World!");
+
+    }
+
+    public static int itreBinarySearch(int[] arr, int key){
+
+    int start = 0;
+    int end = arr.length - 1;
+    int mid = (start + end ) / 2;
+
+
+
+
+
+
+        retunn -1;
+
+
+    }
+
+
+}
