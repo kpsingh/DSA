cc613dccf91176c95ffa435afc131e1901a9f3e0
Krishna Pratap Singh <kpsingh1234@gmail.com>
2022-09-26 23:33:17 +0530

    Update HappyNumber.java

--- src/com/ds/linkedlist/questions/HappyNumber.java
+++ src/com/ds/linkedlist/questions/HappyNumber.java
@@ -1,13 +1,62 @@
 package com.ds.linkedlist.questions;
+
+import java.util.HashMap;
+
 /*
 https://leetcode.com/problems/happy-number/
 
 https://leetcode.com/problems/happy-number/submissions/
 
 
-
  */
 public class HappyNumber {
 
+	public static void main(String[] args) {
+
+		HappyNumber hn = new HappyNumber();
+		boolean isHappy = hn.isHappy(19);
+		System.out.println(isHappy);
+
+	}
+
+	public boolean isHappy(int n) {
+
+		if (n == 0)
+			return false;
+
+		HashMap<Integer, Boolean> map = new HashMap<>();
+
+		while (true) {
+
+			map.put(n, true);
+
+			n = getNext(n);
+
+			if (n == 1)
+				return true;
+
+			if (map.containsKey(n))
+				return false;
+			else
+				map.put(n, true);
+
+		}
+
+		return true;
+
+	}
+
+	private int getNext(int n) {
+
+		int ans = 0;
+
+		while (n != 0) {
+			int r = n % 10;
+			ans = ans + (r * r);
+			n = n / 10;
+		}
+
+		return ans;
+	}
 
 }
