a1d628f6a05f8c7f90ccf4db5e8fb73acb933fb8
Krishna Pratap Singh <kpsingh1234@gmail.com>
2022-10-08 21:53:15 +0530

    Update ReverseLinkedList_206.java

--- src/com/ds/linkedlist/questions/ReverseLinkedList_206.java
+++ src/com/ds/linkedlist/questions/ReverseLinkedList_206.java
@@ -1,5 +1,7 @@
 package com.ds.linkedlist.questions;
 
+import java.util.Stack;
+
 /**
  * https://leetcode.com/problems/reverse-linked-list/
  *
@@ -7,7 +9,7 @@ package com.ds.linkedlist.questions;
 
 public class ReverseLinkedList_206 {
 
-	public ListNode reverseList(ListNode head) {
+	public ListNode reverseListIterative(ListNode head) {
 		if (null == head || head.next == null)
 			return head;
 
@@ -23,6 +25,60 @@ public class ReverseLinkedList_206 {
 
 		return prev;
 
+	}
+
+	ListNode head = null;
+	ListNode tail = null;
+
+	public ListNode reverseListRecursive(ListNode node) {
+
+		if (node == null || node.next == null)
+			return node;
+
+		revese(node);
+
+		tail.next = null;
+		return head;
+	}
+
+	private void revese(ListNode node) {
+		if (node.next == null) {
+			head = node;
+			tail = node;
+		}
+
+		ListNode prev = node;
+		revese(node.next);
+		tail.next = prev;
+		tail = tail.next;
+
+	}
+
+	public ListNode reverseListByStack(ListNode head) {
+
+		if (null == head || head.next == null)
+			return head;
+
+		Stack<ListNode> stack = new Stack<>();
+
+		ListNode temp = head;
+
+		while (temp.next != null) {
+			stack.push(temp);
+			temp = temp.next;
+		}
+
+		head = temp;
+
+		while (!stack.isEmpty()) {
+			temp.next = stack.pop();
+			temp = temp.next;
+
+		}
+
+		temp.next = null;
+
+		return head;
 	}
 
 }
