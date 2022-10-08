72912e890f339d0230b653f74eb8c365b52fdf68
Krishna Pratap Singh <kpsingh1234@gmail.com>
2022-10-08 09:44:13 +0530

    Middle of the Link List

---
+++ src/com/ds/linkedlist/questions/ListNode.java
@@ -0,0 +1,10 @@
+package com.ds.linkedlist.questions;
+
+public class ListNode {
+	int val;
+	ListNode next;
+
+	ListNode(int val) {
+		this.val = val;
+	}
+}
---
+++ src/com/ds/linkedlist/questions/MiddleoftheLinkedList_876.java
@@ -0,0 +1,32 @@
+package com.ds.linkedlist.questions;
+
+/*
+https://leetcode.com/problems/middle-of-the-linked-list/
+
+https://leetcode.com/problems/middle-of-the-linked-list/submissions/
+
+
+ */
+public class MiddleoftheLinkedList_876 {
+
+	public ListNode middleNode(ListNode head) {
+
+		/*
+		 * with the help of slow and fast pointer we can solve this problem. once the
+		 * fast pointer reched to null or its next is null then at that time where the
+		 * slow pointer reached that will be the middle of the linked list.
+		 */
+
+		ListNode slow = head;
+		ListNode fast = head;
+
+		while (fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+		}
+
+		return slow;
+
+	}
+
+}
