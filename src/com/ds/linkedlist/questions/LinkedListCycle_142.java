be8998d4761fec27ab6cfb921664f26890deda69
Krishna Pratap Singh <kpsingh1234@gmail.com>
2022-10-08 15:59:26 +0530

    Length of Cycle

---
+++ src/com/ds/linkedlist/questions/LinkedListCycle_142.java
@@ -0,0 +1,73 @@
+package com.ds.linkedlist.questions;
+
+/**
+ * https://leetcode.com/problems/linked-list-cycle-ii/description/ *
+ */
+
+public class LinkedListCycle_142 {
+
+	public ListNode detectCycle(ListNode head) {
+
+		if (head == null || head.next == null)
+			return null;
+
+		ListNode slow = head;
+		ListNode fast = head;
+
+		while (fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+			if (slow == fast) {
+				/**
+				 * it mean cycle exit, now we need to figure out where cycle started.
+				 * 
+				 * Slow started from head and fast from where both have meet, they will again
+				 * meet at the start of the cycle
+				 */
+
+				slow = head;
+
+				while (slow != fast) {
+					slow = slow.next;
+					fast = fast.next;
+				}
+
+				return slow;
+			}
+
+		}
+		return null;
+
+	}
+
+	public int detectCycleLength(ListNode head) {
+
+		if (head == null || head.next == null)
+			return 0;
+
+		ListNode slow = head;
+		ListNode fast = head;
+
+		while (fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+
+			if (slow == fast) {
+				// Cycle exist
+
+				int length = 0;
+
+				do {
+					slow = slow.next;
+					length++;
+				} while (slow != fast);
+
+				return length;
+			}
+
+		}
+		return 0; // if no cycle
+
+	}
+
+}
---
+++ src/com/ds/linkedlist/questions/MergeTwoSortedList_21.java
@@ -0,0 +1,64 @@
+package com.ds.linkedlist.questions;
+
+/**
+ * 
+ * https://leetcode.com/problems/merge-two-sorted-lists/description/
+ *
+ */
+public class MergeTwoSortedList_21 {
+
+	public static void main(String[] args) {
+		ListNode list1 = new ListNode(1);
+		ListNode node2 = new ListNode(2);
+		ListNode node3 = new ListNode(4);
+		list1.next = node2;
+		node2.next = node3;
+
+		ListNode list2 = new ListNode(1);
+		ListNode node21 = new ListNode(3);
+		ListNode node22 = new ListNode(4);
+		list2.next = node21;
+		node21.next = node22;
+
+		ListNode ans = mergeTwoLists(list1, list2);
+
+		while (ans != null) {
+			System.out.print(ans.val + " -> ");
+			ans = ans.next;
+		}
+		System.out.println("End");
+
+	}
+
+	public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+		// More advance version
+
+		if (list1 == null || list2 == null)
+			return list1 == null ? list2 : list1;
+
+		ListNode ans = new ListNode(0);
+		ListNode head = ans;
+
+		while (list1 != null && list2 != null) {
+
+			if (list1.val < list2.val) {
+
+				head.next = list1;
+				list1 = list1.next;
+
+			} else {
+				head.next = list2;
+				list2 = list2.next;
+
+			}
+			head = head.next;
+
+		}
+
+		head.next = list1 != null ? list1 : list2;
+
+		return ans.next;
+
+	}
+}
