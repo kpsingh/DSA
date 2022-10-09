54954d5158e7afda140202657c067aba32be0511
Krishna Pratap Singh <kpsingh1234@gmail.com>
2022-10-09 16:27:42 +0530

    Create DeleteMiddleFromList_2095.java

---
+++ src/com/ds/linkedlist/questions/DeleteMiddleFromList_2095.java
@@ -0,0 +1,27 @@
+package com.ds.linkedlist.questions;
+
+public class DeleteMiddleFromList_2095 {
+
+	public ListNode deleteMiddle(ListNode head) {
+		if (head == null)
+			return head;
+
+		ListNode dummy = new ListNode(0);
+		dummy.next = head;
+		ListNode prev = dummy;
+
+		ListNode slow = head;
+		ListNode fast = head;
+
+		while (fast != null && fast.next != null) {
+			prev = slow;
+			slow = slow.next;
+			fast = fast.next.next;
+		}
+
+		prev.next = slow.next; // OR pre.next.next;
+
+		return dummy.next;
+	}
+
+}
