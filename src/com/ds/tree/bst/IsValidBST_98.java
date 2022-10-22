a8accbe6d540f1e36649c28a741898442055f922
Krishna Pratap Singh <kpsingh1234@gmail.com>
2022-10-22 17:59:45 +0530

    Create IsValidBST_98.java

---
+++ src/com/ds/tree/bst/IsValidBST_98.java
@@ -0,0 +1,29 @@
+package com.ds.tree.bst;
+
+import com.ds.tree.bs.TreeNode;
+
+public class IsValidBST_98 {
+
+	public boolean isValidBST(TreeNode root) {
+
+		int min = Integer.MIN_VALUE;
+		int max = Integer.MAX_VALUE;
+
+		return isValidBST(root, min, max);
+
+	}
+
+	private boolean isValidBST(TreeNode root, int min, int max) {
+		if (root == null) {
+			return true;
+		}
+
+		if (root.val < min || root.val > max) {
+			return false;
+		}
+
+		return isValidBST(root.left, min, root.val) && isValidBST(root.right, root.val, max);
+
+	}
+
+}
