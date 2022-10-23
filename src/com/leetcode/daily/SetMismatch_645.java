8b12417047d00ef05f3632b71fb8722bdbb2cbae
Krishna Pratap Singh <kpsingh1234@gmail.com>
2022-10-23 17:21:52 +0530

    Update SetMismatch_645.java

--- src/com/leetcode/daily/SetMismatch_645.java
+++ src/com/leetcode/daily/SetMismatch_645.java
@@ -5,7 +5,34 @@ import java.util.Map;
 
 public class SetMismatch_645 {
 
-	public int[] findErrorNums(int[] nums) {
+	// V1
+	public int[] findErrorNums_V1(int[] nums) {
+
+		Map<Integer, Integer> map = new HashMap<>();
+
+		int[] ans = new int[2];
+
+		for (int i : nums) {
+			map.merge(i, 1, (v1, v2) -> v1 + v2);
+		}
+
+		for (int key = 1; key <= nums.length; key++) {
+
+			int val = map.getOrDefault(key, 0);
+
+			if (val == 2) {
+				ans[0] = key;
+			}
+			if (val == 0) {
+				ans[1] = key;
+			}
+		}
+		return ans;
+	}
+	
+	// V2
+	
+	public int[] findErrorNums_V2(int[] nums) {
 
 		Map<Integer, Integer> map = new HashMap<>();
 
