81c3a906a4cf4ad6321da6d73076f278c19c7880
Krishna Pratap Singh <kpsingh1234@gmail.com>
2022-11-06 11:50:46 +0530

    Create PermutationWithCaseChange.java

---
+++ src/com/algo/recusrsion/PermutationWithCaseChange.java
@@ -0,0 +1,41 @@
+package com.algo.recusrsion;
+/*
+ * https://www.youtube.com/watch?v=J2Er5XceU_I&list=PL_z_8CaSLPWeT1ffjiImo0sYTcnLzo-wY&index=15
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PermutationWithCaseChange {
+
+	public static void main(String[] args) {
+		List<String> input = new ArrayList<>();
+		input.add("a");
+		input.add("b");
+		// input.add("C");
+
+		String output = "";
+
+		permuteWithCaseChange(input, output);
+	}
+
+	private static void permuteWithCaseChange(List<String> input, String output) {
+
+		if (input.size() == 0) {
+			System.out.println(output);
+			return;
+		}
+
+		String s = input.get(0);
+		String op1 = output + s;
+		String op2 = output + s.toUpperCase();
+
+		List<String> newInput = new ArrayList<>(input);
+		newInput.remove(0);
+
+		permuteWithCaseChange(newInput, op1);
+		permuteWithCaseChange(newInput, op2);
+
+	}
+
+}
