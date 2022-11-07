c4e27389cc6aac8f841e776afb6a7b9046e25174
Krishna Pratap Singh <kpsingh1234@gmail.com>
2022-11-07 21:33:05 +0530

    Update SortArrray.java

--- src/com/algo/recusrsion/SortArrray.java
+++ src/com/algo/recusrsion/SortArrray.java
@@ -1,23 +1,61 @@
 package com.algo.recusrsion;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SortArrray {
 
 	public static void main(String[] args) {
-		int[] arr = { 3, 2, 5 };
-		sort(arr, arr.length);
-		Arrays.toString(arr);
+
+		List<Integer> list = new ArrayList<>();
+		list.add(5);
+		list.add(2);
+		list.add(3);
+		list.add(12);
+		list.add(1);
+		list.add(25);
+
+		for (int a : list) {
+			System.out.print(a + " ");
+		}
+		System.out.println();
+
+		List<Integer> sort = sort(list);
+
+		for (int a : sort) {
+			System.out.print(a + " ");
+		}
+		System.out.println();
+
 	}
 
-	private static void sort(int[] arr, int size) {
-
-		if (size == 1) {
-			return;
+	private static List<Integer> sort(List<Integer> list) {
+		if (list.size() == 1) {
+			return list;
 		}
 
-		sort(arr, size--);
+		int temp = list.get(list.size() - 1);
 
+		List<Integer> subList = list.subList(0, list.size() - 1);
+
+		subList = sort(subList);
+
+		return merge(subList, temp);
+
+	}
+
+	private static List<Integer> merge(List<Integer> list, int target) {
+
+		if (list.size() == 0 || list.get(list.size() - 1) <= target) {
+			list.add(target);
+			return list;
+		}
+
+		int temp = list.get(list.size() - 1);
+
+		List<Integer> subLis = merge(list.subList(0, list.size() - 1), target);
+		subLis.add(temp);
+		return subLis;
 	}
 
 }
