4e689bd91f1d9027e5ea97dc6b2ebac5344706fc
Krishna Pratap Singh <kpsingh1234@gmail.com>
2023-03-18 22:50:37 +0530

    Update LCS.java

--- src/com/algo/dp/LCS.java
+++ src/com/algo/dp/LCS.java
@@ -7,8 +7,8 @@ public class LCS {
 
 	public static void main(String[] args) {
 
-		String s1 = "bsbininm";
-		String s2 = "jmjkbkjkv";
+		String s1 = "bsbininm"; // pmjghexybyrgzczy, bsbininm
+		String s2 = "jmjkbkjkv"; // hafcdqbgncrcbihkd, jmjkbkjkv
 
 		System.out.println(longestCommonSubsequence(s1, s2));
 
@@ -21,7 +21,7 @@ public class LCS {
 		s1 = text1;
 		s2 = text2;
 
-		// return lcs_rec(0, 0);
+		//return lcs_rec(0, 0);
 		return lcs_DP(text1, text2);
 	}
 
@@ -45,7 +45,7 @@ public class LCS {
 	 */
 
 	private static int lcs_DP(String s1, String s2) {
-		
+
 		int n = s1.length();
 		int m = s2.length();
 
@@ -56,15 +56,20 @@ public class LCS {
 		int[][] dp = new int[n + 1][m + 1];
 
 		for (int i = 1; i <= n; i++) {
+
 			for (int j = 1; j <= m; j++) {
 
 				if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+
 					dp[i][j] = 1 + Math.max(dp[i - 1][j], dp[i][j - 1]);
+
 				} else {
+
 					dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
 				}
 			}
 		}
+
 		return dp[n][m];
 	}
 
